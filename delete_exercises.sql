0fb67fa18104afa835ff50806c742e1b0f36c263
Briana Villanueva <briana@Brianas-MacBook-Air.local>
2020-02-26 11:22:19 -0600

    data

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,7 +1,14 @@
+
 USE codeup_test_db;
 
-DELETE FROM albums WHERE release_date >= 1991;
+SELECT 'The Albums Released after 1991 => ', record_name, release_date FROM albums WHERE release_date > '1991';
+DELETE FROM albums WHERE release_date > '1991';
 
-DELETE FROM albums WHERE  genre = 'disco';
+SELECT 'The Albums of Genre ''Disco'' => ', record_name, genre FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE genre = 'disco';
 
-DELETE FROM albums WHERE artist = 'Whitney Houston';
+SELECT 'All Whitney Houston Albums in Table => ', record_name, artist_name FROM albums WHERE artist_name = 'Whitney Houston';
+DELETE FROM albums WHERE artist_name = 'Whitney Houston';
+
+SELECT 'All Albums in Table => ', artist_name, record_name, release_date, genre FROM albums;
+© 2020 GitHub, Inc.
